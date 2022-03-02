6d900bb24a9f89a7bf88b34b2d4732dc100b2932
Natapat Samutpong <natapat.samutpong@gmail.com>
2022-03-02 07:58:24 +0700

    remove test function

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,6 @@ import { Backend } from "./Backend";
 import dotenv from "dotenv";
 import { Logger } from "./util/logger";
 dotenv.config();
-Logger.info("test", "abc");
-Logger.warn("omg!!", "warning: error :(");
-Logger.error("skill");
 
 async function main() {
   const backend = await Backend.create();
